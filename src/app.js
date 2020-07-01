487f7f4178690ace5f8de2814846757b03783149
Roman-Kirnos <kirnos007@gmail.com>
2020-07-01 14:18:36 +0300

    second commit: add http server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
+const http = require('http');
+
 const log = require('./logger')(__filename);
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end();
+});
+
+server.on('clientError', (err, socket) => {
+  log.error('HTTP error');
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
 
 function launch() {
   log.info('App start to working now!');
+
+  server.listen(PORT);
+
+  log.info(`PORT: ${PORT}`);
 }
 
 module.exports = launch;
