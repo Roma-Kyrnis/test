8a8fa1e338e194d13e699ab59d42ee07a1e8ed9a
Roman-Kirnos <kirnos007@gmail.com>
2020-07-01 14:18:36 +0300

    add http server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
+const http = require('http');
+
 const log = require('./logger')(__filename);
+
+const server = http.createServer((req, res) => {
+  res.end();
+});
+
+server.on('clientError', (err, socket) => {
+  log.error('HTTP error');
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
 
 function launch() {
   log.info('App start to working now!');
+
+  server.listen(3000);
+
+  log.info('PORT: 3000');
 }
 
 module.exports = launch;
